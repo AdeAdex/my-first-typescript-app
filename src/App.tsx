2f517594ab2e0c56271a2795ad78d37004bce098
Adeolu Amole <adeoluamole@gmail.com>
2023-11-28 13:15:19 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+interface About {
+  firstname: string;
+  lastname: string;
+  age: number;
+  title: string;
+}
 
 function App() {
+  const info: About = {
+    firstname: "Adeolu",
+    lastname: "Amole",
+    age: 0,
+    title: "Software Engineer",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="bg-body-background max-w-screen-lg mx-auto mt-8">
+        <h2 className="text-center text-3xl font-bold mb-4">
+          User Information
+        </h2>
+        <table className="table-auto w-full">
+          <thead>
+            <tr className="bg-gray-200">
+              <th className="p-2">Firstname</th>
+              <th className="p-2">Lastname</th>
+              <th className="p-2">Age</th>
+              <th className="p-2">Title</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="hover:bg-gray-200 transition duration-300 text-center">
+              <td className="p-2">{info.firstname}</td>
+              <td className="p-2">{info.lastname}</td>
+              <td className="p-2">{info.age}</td>
+              <td className="p-2">{info.title}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'body-background': 'whiteSmoke',
+      }
+    },
+  },
+  plugins: [],
+}
+
